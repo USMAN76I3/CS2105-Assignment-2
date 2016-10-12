10b943116b626c95c0846e2fd7a6b0d3a54933bb
sherinatoh <sherinatohh@gmail.com>
2016-10-13 02:33:10 +0800

    working version of non >1000 size message

--- Receiver.java
+++ Receiver.java
@@ -1,43 +1,100 @@
 import java.net.*;
 import java.nio.*;
-
-
+import java.util.Arrays;
+import java.util.zip.CRC32;
 
 class Receiver {
-  public static void main(String[] args) throws Exception {
-    // check if the number of command line argument is 4
-    if (args.length != 1) {
-      System.out.println("Usage: java Receiver <port>");
-      System.exit(1);
-    }
-    new Receiver(Integer.parseInt(args[0]));
-  }
-  
-  public Receiver(int port) throws Exception {
-    // Do not change this
-    DatagramSocket socket = new DatagramSocket(port);
-    while (true) {
-      String message = receiveMessage(socket);
-      System.out.println(message);
-    }
-  }
-  
-  public String receiveMessage(DatagramSocket socket) throws Exception {
-      // Implement me
-	  
-	  byte[] inBuffer = new byte[1024];
-	  DatagramPacket receivedPacket = new DatagramPacket(inBuffer, inBuffer.length);
-	  socket.receive(receivedPacket);
-	  
-	  String ACKED = "ACK";
-	  String receivedData = new String(receivedPacket.getData(), 0, receivedPacket.getLength());
-	  InetAddress senderAddress = receivedPacket.getAddress();
-	  int senderPort = receivedPacket.getPort();
-	  
-	  byte[] outBuffer = ACKED.getBytes();
-	  DatagramPacket sendPacket = new DatagramPacket(outBuffer, outBuffer.length, senderAddress, senderPort);
-	  
-	  socket.send(sendPacket);
-	  return receivedData;
-  }
+	public static void main(String[] args) throws Exception {
+		// check if the number of command line argument is 4
+		if (args.length != 1) {
+			System.out.println("Usage: java Receiver <port>");
+			System.exit(1);
+		}
+		new Receiver(Integer.parseInt(args[0]));
+	}
+
+	public Receiver(int port) throws Exception {
+		// Do not change this
+		DatagramSocket socket = new DatagramSocket(port);
+		while (true) {
+			String message = receiveMessage(socket);
+			System.out.println(message);
+		}
+	}
+
+	public long getChecksum(byte[] message) {
+		CRC32 crc = new CRC32();
+		crc.update(message);
+		return crc.getValue();
+	}
+	
+	public long bytesToLong(byte[] bytes) {
+		ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+		buffer.put(bytes);
+		buffer.flip();
+		return buffer.getLong();
+	}
+
+	public boolean verifyCS(byte[] message, int size) {
+		byte[] messageBytes = Arrays.copyOfRange(message, 0, size - 8);
+		byte[] checksumBytes = Arrays.copyOfRange(message, size - 8, size);
+		long messageCS = getChecksum(messageBytes);
+		long checksum = bytesToLong(checksumBytes);
+		return messageCS == checksum;
+	}
+	
+	public int getSequenceNumber(byte[] message, int size) {
+		byte[] seqNumArr = Arrays.copyOfRange(message, size - 12, size -8);
+		return ByteBuffer.wrap(seqNumArr).getInt();
+
+	}
+	
+	int sequenceNumber = 0;
+
+	public String receiveMessage(DatagramSocket socket) throws Exception {
+		// Implement me
+
+		byte[] inBuffer = new byte[1008];
+		DatagramPacket receivedPacket = new DatagramPacket(inBuffer, inBuffer.length);
+		socket.receive(receivedPacket);
+		// Packet Received
+		
+		int packetSize = receivedPacket.getLength();
+		byte[] packetData = receivedPacket.getData();
+		InetAddress senderAddress = receivedPacket.getAddress();
+		int senderPort = receivedPacket.getPort();
+		//System.out.println(verifyCS(packetData, packetSize));
+		
+		if (verifyCS(packetData, packetSize)) {
+			String receivedData = new String(receivedPacket.getData(), 0, packetSize - 12);
+			int seqNum = getSequenceNumber(receivedPacket.getData(), packetSize);
+			
+			if (seqNum == sequenceNumber) {
+				String ACKED = "ACK";
+				byte[] outBuffer = ACKED.getBytes();
+				DatagramPacket sendPacket = new DatagramPacket(outBuffer, outBuffer.length, senderAddress, senderPort);
+
+				socket.send(sendPacket);
+				return receiveMessage(socket);
+			}
+
+			String ACKED = "ACK";
+			byte[] outBuffer = ACKED.getBytes();
+			DatagramPacket sendPacket = new DatagramPacket(outBuffer, outBuffer.length, senderAddress, senderPort);
+			sequenceNumber = seqNum;
+
+			socket.send(sendPacket);
+			return receivedData;
+		} else {
+			String NAKED = "NAK";
+			byte[] outBuffer = NAKED.getBytes();
+			
+			DatagramPacket sendPacket = new DatagramPacket(outBuffer, outBuffer.length, senderAddress, senderPort);
+			
+			socket.send(sendPacket);
+			return receiveMessage(socket);
+		}
+		
+		 //should never reach here
+	}
 }
--- Sender.java
+++ Sender.java
@@ -1,6 +1,8 @@
 import java.net.*;
 import java.nio.*;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.zip.CRC32;
 
 class Sender {
 	public static void main(String[] args) throws Exception {
@@ -24,6 +26,52 @@ class Sender {
 		}
 	}
 
+	public byte[] longToByte(long value) {
+		ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+		buffer.putLong(value);
+		return buffer.array();
+	}
+
+	public long getChecksum(byte[] message) {
+		CRC32 crc = new CRC32();
+		crc.update(message);
+		return crc.getValue();
+	}
+
+	public byte[] splitMessage(byte[] message, int start, int end) {
+		byte[] packetSize = Arrays.copyOfRange(message, start, end);
+		return packetSize;
+	}
+	
+	private byte[] concatenate(byte[] buffer1, byte[] buffer2) {
+		byte[] returnBuffer = new byte[buffer1.length + buffer2.length];
+		System.arraycopy(buffer1, 0, returnBuffer, 0, buffer1.length);
+		System.arraycopy(buffer2, 0, returnBuffer, buffer1.length, buffer2.length);
+		return returnBuffer;
+	}
+	
+	public long bytesToLong(byte[] bytes) {
+		ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+		buffer.put(bytes);
+		buffer.flip();
+		return buffer.getLong();
+	}
+	
+	public byte[] getMessageType(int num) {
+		ByteBuffer typeByteArr = ByteBuffer.allocate(4);
+		typeByteArr.putInt(num);
+		return typeByteArr.array();
+	}
+	
+	public byte[] getSequenceNumber() {
+		ByteBuffer seqByteArr = ByteBuffer.allocate(4);
+		sequenceNumber++;
+		seqByteArr.putInt(sequenceNumber);
+		return seqByteArr.array();
+	}
+	
+	int sequenceNumber = 0;
+
 	public void sendMessage(String message, String host, int port) throws Exception {
 		// You can assume that a single message is shorter than 750 bytes and
 		// thus
@@ -31,28 +79,68 @@ class Sender {
 		// Implement me
 
 		DatagramSocket clientSocket = new DatagramSocket();
+		InetAddress IPAddress = InetAddress.getByName(host);
 
 		byte[] messageData = message.getBytes();
-		InetAddress IPAddress = InetAddress.getByName(host);
-		DatagramPacket sendPacket = new DatagramPacket(messageData, messageData.length, IPAddress, port);
+		int messageLength = messageData.length;
+		int totalPackets = (messageLength / 1000) + 1;
 
-		clientSocket.send(sendPacket);
+		if (totalPackets == 1) {
+			
+			byte[] msgAndSeq = concatenate(messageData, getSequenceNumber());
+			long checksum = getChecksum(msgAndSeq);
+			byte[] checksumByte = longToByte(checksum);
+			//System.out.println(bytesToLong(checksumByte));
+			byte[] updatedMessage = concatenate(msgAndSeq, checksumByte);
+			DatagramPacket sendPacket = new DatagramPacket(updatedMessage, updatedMessage.length, IPAddress, port);
+			
+			clientSocket.send(sendPacket);
 
-		byte[] inBuffer = new byte[1024];
-		DatagramPacket receivedPacket = new DatagramPacket(inBuffer, inBuffer.length);
+			byte[] inBuffer = new byte[1024];
+			DatagramPacket receivedPacket = new DatagramPacket(inBuffer, inBuffer.length);
 
-		boolean NOTACKED = true;
-		while (NOTACKED) {
-			try {
-				clientSocket.setSoTimeout(2000);
-				clientSocket.receive(receivedPacket);
-				
-				String ackedMsg = new String(receivedPacket.getData(), 0, receivedPacket.getLength());
-				if (ackedMsg.equals("ACK")) {
-					NOTACKED = false;
+			boolean NOTACKED = true;
+			while (NOTACKED) {
+				try {
+					clientSocket.setSoTimeout(2000);
+					clientSocket.receive(receivedPacket);
+
+					String ackedMsg = new String(receivedPacket.getData(), 0, receivedPacket.getLength());
+					if (ackedMsg.equals("ACK")) {
+						NOTACKED = false;
+					} else if (ackedMsg.equals("NAK")) {
+						clientSocket.send(sendPacket);
+					} else {
+						clientSocket.send(sendPacket);
+					}
+				} catch (SocketTimeoutException e) {
+					clientSocket.send(sendPacket);
 				}
-			} catch (SocketTimeoutException e) {
+			}
+		} else {
+			for (int i = 0; i < totalPackets - 1; i++) {
+
+				DatagramPacket sendPacket = new DatagramPacket(messageData, messageData.length, IPAddress, port);
+
 				clientSocket.send(sendPacket);
+
+				byte[] inBuffer = new byte[1024];
+				DatagramPacket receivedPacket = new DatagramPacket(inBuffer, inBuffer.length);
+
+				boolean NOTACKED = true;
+				while (NOTACKED) {
+					try {
+						clientSocket.setSoTimeout(2000);
+						clientSocket.receive(receivedPacket);
+
+						String ackedMsg = new String(receivedPacket.getData(), 0, receivedPacket.getLength());
+						if (ackedMsg.equals("ACK")) {
+							NOTACKED = false;
+						}
+					} catch (SocketTimeoutException e) {
+						clientSocket.send(sendPacket);
+					}
+				}
 			}
 		}
 
